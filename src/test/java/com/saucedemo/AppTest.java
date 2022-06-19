0c804ea9c93341a9ea808233d0578549fabb185d
walidferhat89 <walidferhat89@gmail.com>
2022-06-19 16:53:50 -0400

    basic Junit framework, logs, seperate setUp,tearDown/tests

--- src/test/java/com/saucedemo/AppTest.java
+++ src/test/java/com/saucedemo/AppTest.java
@@ -1,6 +1,10 @@
 package com.saucedemo;
 
+
+import org.junit.After;
 import org.junit.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,35 +12,28 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
-    /*public void setUp(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\walid\\IdeaProjects\\may-22-experteam-bootcamp-framework\\driver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-        Assert.assertEquals("Swag Labs", driver.getTitle());
-    }*/
 
-    @Test
-    public void test1 () {
+    WebDriver driver;
+    @Before
+    public void setUp(){
+
         System.setProperty("webdriver.chrome.driver","C:\\Users\\walid\\IdeaProjects\\may-22-experteam-bootcamp-framework\\driver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
-        String title=driver.getTitle();
-        System.out.println(title);
-        Assert.assertEquals("Swag Labs",title);
+        System.out.println("the title: " +driver.getTitle());
+        Assert.assertEquals("Swag Labs", driver.getTitle());
+    }
+    @After
+    public void tearDown(){
         driver.close();
     }
 
+
+
     @Test
     public void test2 (){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\walid\\IdeaProjects\\may-22-experteam-bootcamp-framework\\driver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-        String title=driver.getTitle();
-        System.out.println("title appears : " + title);
-        Assert.assertEquals("Swag Labs",title);
+
 
         WebElement userNameField = driver.findElement(By.id("user-name"));
         boolean userNameIsDisplayed = userNameField.isDisplayed();
@@ -52,33 +49,29 @@ public class AppTest {
         boolean loginbtnIsDisplayed = loginBtn.isDisplayed();
         System.out.println("login btn is displayed: " + loginbtnIsDisplayed);
         Assert.assertTrue(loginbtnIsDisplayed);
-        driver.close();
 
     }
     @Test
     public void test3(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\walid\\IdeaProjects\\may-22-experteam-bootcamp-framework\\driver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-        String title=driver.getTitle();
-        System.out.println("title appears : " + title);
-        Assert.assertEquals("Swag Labs",title);
 
         WebElement usernameField = driver.findElement(By.id("user-name"));
         usernameField.sendKeys("standard_user");
         System.out.println("User name entered,");
+
         WebElement passwordField = driver.findElement(By.id("password"));
         passwordField.sendKeys("secret_sauce");
         System.out.println("password entered,");
+
         WebElement clickBtn = driver.findElement(By.id("login-button"));
         clickBtn.click();
         System.out.println("login btn clicked");
+
         WebElement Header = driver.findElement(By.className("title"));
         String getTex = Header.getText();
         System.out.println("headerTitle:" + getTex);
         Assert.assertEquals("PRODUCTS",getTex);
-        driver.close();
+
+
 
     }
 }
